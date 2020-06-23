dcd52816f2a55ee188342ca9f348018024fb1599
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-24 06:30:03 +0700

    Create 7. Loops 2.py

---
+++ 7. Loops 2.py
@@ -0,0 +1,18 @@
+# 7.1 For
+
+# for a in range(1, 3):
+#     print(a)
+
+# 7.2 Break (Stop)
+
+# for a in range(1, 3):
+#     if a == 2:
+#         break
+#     print(a)
+
+# 7.3 Continue (Stop)
+
+# for a in range(1, 3):
+#     if a == 2:
+#         continue
+#     print(a)
