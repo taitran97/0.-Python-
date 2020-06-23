662f75baa4738049b7ece6a6035a354ade1a08b0
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-24 06:29:57 +0700

    Create 6. Loops 1.py

---
+++ 6. Loops 1.py
@@ -0,0 +1,28 @@
+# 6.1 While (When)
+
+# a = 0
+# (a ↑ 0 and a < 3)
+# while a < 3:
+#     print(a)
+#     a += 1
+
+# 6.2 Break (Stop)
+
+# a = 0
+# (a ↑ 0 and a == 3)
+# while a < 5:
+#     print(a)
+#     if a == 3:
+#         break
+#     a += 1
+
+# 6.3 Continue [Delete]
+
+# a = 0
+# ([a ↑ 1 and a == 3] and a <= 5)
+# while a < 5:
+#     a += 1
+#     if a == 3:
+#         continue
+#     print(a)
+
