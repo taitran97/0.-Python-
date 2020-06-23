7514bf82553584c8877f775b7c32062268ae4638
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-24 06:29:48 +0700

    Create 3. Input.py

---
+++ 3. Input.py
@@ -0,0 +1,12 @@
+# 3.1 Input
+
+# <class 'str'>
+# inputAnything = input()
+# print("Output: " + inputAnything)
+
+# 3.4 Change Type
+
+# <class 'str'> --> <class 'int'>
+# changeType = int(input())
+# changeType = changeType + 10
+# print(changeType)
