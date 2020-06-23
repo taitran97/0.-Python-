629997d70eaf832e90b4f19bd09d48b031801284
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-24 06:30:06 +0700

    Create 8. List.py

---
+++ 8. List.py
@@ -0,0 +1,15 @@
+# 8.1 List (Change)
+
+# acMilan = ["Alexander", "Maldini", "Nesta"]
+# print(acMilan)
+
+# 8.2 Access Items
+
+# acMilan = ["Alexander", "Maldini", "Nesta"]
+# print(acMilan[0])
+
+# 8.3 Remove Items
+
+# acMilan = ["Alexander", "Maldini", "Nesta", "Cafu"]
+# acMilan.remove("Cafu")
+# print(acMilan)
