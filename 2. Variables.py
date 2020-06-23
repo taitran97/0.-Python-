49398d8a8db33da4592e8e26870dcca1d2d9a632
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-24 06:29:45 +0700

    Create 2. Variables.py

---
+++ 2. Variables.py
@@ -0,0 +1,35 @@
+# 2.1 Variables
+
+# nameVariables = "Alexander"
+# print(nameVariables)
+
+# 2.2 Strings
+
+# myStrings = "Hello Python"
+# print(myStrings)
+
+# 2.1 Numbers (Integers)
+
+# myInteger = 5
+# print(myInteger)
+
+# myFloat = 5.0
+# print(myFloat)
+
+# 2.3 Boolean
+
+# myBoolean = True
+# print(myBoolean)
+
+# 2.4 Data Type
+
+# a = 3
+# print(type(a))
+
+# 2.5 String Upper
+
+# stringUpper = "Hello World"
+# print(stringUpper.upper())
+
+stringUpper = "Hello World"
+print(stringUpper.upper())
