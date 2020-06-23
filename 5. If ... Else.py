af79cdc205e92b3750ccf3464fd180088fc695ac
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-24 06:29:54 +0700

    Create 5. If ... Else.py

---
+++ 5. If ... Else.py
@@ -0,0 +1,24 @@
+# 5.1 If
+
+# a = 5
+# b = 3
+# if a > b:
+#     print("a > b")
+
+# 5.2 Else (Different)
+
+# a = 5
+# b = 3
+# if b > a:
+#     print("b > a")
+# else:
+#     print("a > b")
+
+# 5.3 Elif
+
+# a = 3
+# b = 3
+# if a > b:
+#     print("a > b")
+# elif a == b:
+#     print("a == b")
