01f7fd745a96d673ad1be5bb1158cc85d496e7b7
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-26 06:48:39 +0700

    Update 4. Operators.py

--- 4. Operators.py
+++ 4. Operators.py
@@ -1,37 +1,37 @@
-# 4.1 Arithmetic Operators
+# 1. Arithmetic Operators
 
 # x = 3
 # y = 3
 # print(x + y)
 
-# 4.2 Assignment Operators
+# 2. Assignment Operators
 
 # x = x + 3
 # x = 3
 # x += 3
 # print(x)
 
-# 4.3 Comparison Operators
+# 3. Comparison Operators
 
 # x = 3
 # y = 3
 # print(x == y)
 
-# 4.4 Logical Operators
+# 4. Logical Operators
 
 # True if both statements are true.
 # x = True
 # y = True
 # print(x and y)
 
-# 4.5 Identity Operators
+# 5. Identity Operators
 
 # True if both variables are the same object.
 # x = ("Alexander", "Maldini", "Nesta")
 # y = ("Alexander", "Maldini", "Nesta")
 # print(x is y)
 
-# 4.6 Membership Operators
+# 6. Membership Operators
 
 # True if a sequence with the specified value is present in the object.
 # x = ("Alexander", "Maldini", "Nesta")
