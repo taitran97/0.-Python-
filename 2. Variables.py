b4ca00adf7f1454e2711662c909adb2cf1a88800
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-25 07:16:51 +0700

    Update 2. Variables.py

--- 2. Variables.py
+++ 2. Variables.py
@@ -1,35 +1,22 @@
-# 2.1 Variables
-
+# 1. Variables
 # nameVariables = "Alexander"
 # print(nameVariables)
 
-# 2.2 Strings
-
+# 2. Strings
 # myStrings = "Hello Python"
 # print(myStrings)
 
-# 2.1 Numbers (Integers)
-
+# 3. Numbers
 # myInteger = 5
 # print(myInteger)
 
 # myFloat = 5.0
 # print(myFloat)
 
-# 2.3 Boolean
-
+# 4. Boolean
 # myBoolean = True
 # print(myBoolean)
 
-# 2.4 Data Type
-
+# 5. Data Type
 # a = 3
 # print(type(a))
-
-# 2.5 String Upper
-
-# stringUpper = "Hello World"
-# print(stringUpper.upper())
-
-stringUpper = "Hello World"
-print(stringUpper.upper())
